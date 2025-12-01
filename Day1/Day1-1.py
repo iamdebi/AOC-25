41e2d8ac8dda5ec721bde876a3508dab822cbcc7
debbieurquhart <debiurquhart@live.co.uk>
2025-12-01 21:43:42 +0000

    solves day 1 part 1

--- Day1/Day1-1.py
+++ Day1/Day1-1.py
@@ -1,42 +1,65 @@
 
-inputName = "/Users/debbieurquhart/code_projects/AOC-25/Day1/example"
+inputName = "/Users/debbieurquhart/code_projects/AOC-25/Day1/input1"
 
 f = open(inputName)
 current = 50 
 hit0 = 0
+lineNumber = 1
 
-def is_over_99(currentPosition):
-    if currentPosition > 99:
-        amount = currentPosition - 99
-        print(f"decreasing current by {amount}, currentPosition = {currentPosition}")
-        currentPosition = 0 + amount - 1
-        return currentPosition
-    else:
-        return currentPosition
-    
-def is_less_0(currentPosition):
-    if currentPosition < 0:
-        amount = 0 - currentPosition
-        print(f"increasing current by {amount}, currentPosition = {currentPosition}")
-        currentPosition = 99 - amount + 1
-        return currentPosition
-    else:
-        return currentPosition
+def is_greater_than_99(pos):
+    while pos > 99:
+        pos = pos - 100
+    return pos
+
+def is_less_than_0(pos):
+    while pos < 0:
+        pos = pos + 100
+    return pos
+
+def is_more_than_100(amount):
+    # This is equivalent to: amount % 100
+    return amount % 100 
+        
 
 for line in f:
+    print("---------------------")
+    print(f"parsing line no {lineNumber}, contains: {line}")
+    
     direction = line[0]
-    amount = int(line[1:])
-    if direction == "L":
-        current -= amount
-        current = is_less_0(current)
+    toMove = int(line[1:])
+    amount = 0
+    if toMove > 100:
+        print(f"greater than 100, to move {toMove}")
+        amount = is_more_than_100(toMove)
+        print(f"amount: {amount}")
     else:
-        current += amount
-        current = is_over_99(current)
+        amount = toMove
         
-    print(current)
+    if direction == "L":
+        print(f"current: {current}, amount: {amount}")
+        newPos = current - amount
+        newCur = is_less_than_0(newPos)
+        print(f"newPos: {newPos}, newCur: {newCur}")
+        current = newCur
+
+    elif direction == "R":
+        newPos = current + amount
+        current = is_greater_than_99(newPos)
+
     if current == 0:
-        print("Hit 0")
         hit0 +=1
 
+    if current < 0:
+        print(f"currentl has exceed 0, {lineNumber}:, {line}")
+        break
+
+    lineNumber +=1
+    print(f"finished pasring line {line}, current: {current} of type {type(current)}")
+    print("---------------------")
+
 print(hit0)
+
+# print(10 + -50) = -40
+
+
 f.close()
