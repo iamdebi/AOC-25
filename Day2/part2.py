1c72e2568797a59eafad2bcb155716343c9b223b
debbieurquhart <debiurquhart@live.co.uk>
2025-12-02 09:30:10 +0000

    solves day2 part 2

--- Day2/part2.py
+++ Day2/part2.py
@@ -1,41 +1,53 @@
-inputName = "/Users/debbieurquhart/code_projects/AOC-25/Day1/input1"
+import math
 
-current = 50
-part2 = 0   # count any click that lands on 0 (part 2)
+inputName = "/Users/debbieurquhart/code_projects/AOC-25/Day2/input"
+f = open(inputName).read()
 
-with open(inputName) as f:
-    for line in f:
-        line = line.strip()
-        if not line:
-            continue
-        direction = line[0]
-        steps = int(line[1:])
+rangesArray = f.split(",")
+ranges = []
+for idRange in rangesArray:
+    id1, id2 = idRange.split("-")
+    ranges.append((int(id1), int(id2)))
 
+MAX_DIGITS = 12
+seen = set()
+total = 0
 
-# If you want part2 you need to re-run and simulate counts of clicks:
-current = 50
-with open(inputName) as f:
-    for line in f:
-        line = line.strip()
-        if not line:
-            continue
-        direction = line[0]
-        steps = int(line[1:])
-        d = 1 if direction == "R" else -1
+for (low, high) in ranges:
 
-        # compute first positive k where the dial hits 0
-        k0 = (-current * d) % 100
-        if k0 == 0:
-            k0 = 100
+    for d in range(1, MAX_DIGITS + 1):        
+        X_min = 10**(d - 1)                   
+        X_max = 10**d - 1                     
 
-        if steps >= k0:
-            hits = 1 + (steps - k0) // 100
-        else:
-            hits = 0
+        for R in range(2, MAX_DIGITS + 1):
+            total_digits = d * R
+            if total_digits > MAX_DIGITS:
+                break
 
-        part2 += hits
+            
+            mask = 0
+            for i in range(R):
+                mask += 10**(i * d)
 
-        # update position after the whole rotation
-        current = (current + d * steps) % 100
+            A_low  = math.ceil(low  / mask)
+            A_high = math.floor(high / mask)
 
-print("Part 2 (every click zeros):", part2)
+            if A_low < X_min:
+                A_low = X_min
+            if A_high > X_max:
+                A_high = X_max
+            if A_low > A_high:
+                continue
+
+            for X in range(A_low, A_high + 1):
+                N = X * mask
+
+                if len(str(N)) != total_digits:
+                    continue
+
+                if low <= N <= high:
+                    if N not in seen:  
+                        seen.add(N)
+                        total += N
+
+print(total)
