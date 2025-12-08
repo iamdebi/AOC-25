dc4f02b65cc57f86fdc39d51e7d03be1337cb103
debbieurquhart <debiurquhart@live.co.uk>
2025-12-08 09:33:44 +0000

    solves Day8 part 1

--- Day8/part2.py
+++ Day8/part2.py
@@ -1,32 +1,73 @@
+import math
 
-inputName = "/Users/debbieurquhart/code_projects/AOC-25/Day8/input"
 # inputName = "/Users/debbieurquhart/code_projects/AOC-25/Day8/example"
-f = open(inputName)
+inputName = "/Users/debbieurquhart/code_projects/AOC-25/Day8/input"
 
-lineNumber = 1
+points = []
 
-sum = 0
+with open(inputName) as f:
+    for line in f:
+        line = line.strip()
+        x,y,z = line.split(",")
+        points.append((int(x), int(y), int(z)))
 
-for bank in f:
-    print("---------------------")
-    bank = bank.strip()              
+pairs = []
+
+for i in range(len(points)):
+    x1, y1, z1 = points[i]
+    for j in range(i + 1, len(points)):
+        x2, y2, z2 = points[j]
+
+        dist = math.sqrt(
+            (x2 - x1) ** 2 +
+            (y2 - y1) ** 2 +
+            (z2 - z1) ** 2 
+        )
+
+        pairs.append((dist, i, j))
+
+pairs.sort()
+
+parent = []
+size = []
+components = len(points)
+
+for i in range(len(points)):
+    parent.append(i)
+    size.append(1)
+
+def find(x):
+    while parent[x] != x:
+        parent[x] = parent[parent[x]]
+        x = parent[x]
+    return x
+
+def union(a, b):
+    rootA = find(a)
+    rootB = find(b)
+
+    if rootA == rootB:
+        return False
     
-    batteriesOn = []
-    remove = len(bank) - 12
-    parsedBank = [int(battery) for battery in bank]
+    if size[rootA] < size[rootB]:
+        parent[rootA] = rootB
+        size[rootB]+= size[rootA]
+        return True
+    else:
+        parent[rootB] = rootA
+        size[rootA] += size[rootB]
+        return True
 
-    for battery in parsedBank:
-        while len(batteriesOn) > 0 and batteriesOn[-1] < battery and remove > 0:
-            batteriesOn.pop(-1)
-            remove -= 1
-        batteriesOn.append(battery)
+for dist, a, b in pairs:
+    if union(a, b):
+        components -=1
 
-    while len(batteriesOn) > 12:
-        batteriesOn.pop(-1)
+        if components == 1:
+            x1 = points[a][0]
+            x2 = points[b][0]
+            answer = x1 * x2
+            print(answer)
+            break
+            
 
-    joltage = int("".join(str(battery) for battery in batteriesOn))
-
-    sum += joltage
-
-print(f"total sum: {sum}")
-f.close()
+circuits = {}
