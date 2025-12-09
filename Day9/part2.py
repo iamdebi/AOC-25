48e660ce1b1a53ae3e3560ce74349797a8bee8ab
debbieurquhart <debiurquhart@live.co.uk>
2025-12-09 10:38:18 +0000

    solves the example part 2 just times out in the input

--- Day9/part2.py
+++ Day9/part2.py
@@ -1,5 +1,5 @@
-# inputName = "/Users/debbieurquhart/code_projects/AOC-25/Day9/example"
-inputName = "/Users/debbieurquhart/code_projects/AOC-25/Day9/input"
+inputName = "/Users/debbieurquhart/code_projects/AOC-25/Day9/example"
+# inputName = "/Users/debbieurquhart/code_projects/AOC-25/Day9/input"
 
 points = []
 
@@ -14,18 +14,78 @@ with open(inputName) as f:
 
 maxArea = 0
 
+red = set(points)
+green = set()
+
+def isBoundary(x, y):
+    return (x, y) in red or (x, y) in green
+
+for i in range(len(points)):
+    x1, y1 = points[i]
+    x2, y2 = points[(i+1) % len(points)]
+
+    if x1 == x2:
+        direction =  1 if y2 > y1 else -1
+        for y in range(y1 + direction, y2, direction):
+            green.add((x1, y))
+    else:
+        direction =  1 if x2 > x1 else -1
+        for x in range(x1 + direction, x2, direction):
+            green.add((x, y1))
+
+min_x = min(x for x, y in points)
+max_x = max(x for x, y in points)
+min_y = min(y for x, y in points)
+max_y = max(y for x, y in points)
+
+visited = set()
+stack = [(min_x, min_y)]
+
+while stack:
+    x, y = stack.pop()
+    if (x, y) in visited:
+        continue
+    if isBoundary(x, y):
+        continue
+
+    visited.add((x, y))
+
+    if x > min_x : stack.append((x-1, y))
+    if x < max_x : stack.append((x+1, y))
+    if y > min_y : stack.append((x, y-1))
+    if y < max_y : stack.append((x, y+1))
+
+for y in range(min_y, max_y + 1):
+    for x in range(min_x, max_x + 1):
+        if (x, y) not in visited and (x, y) not in red:
+            green.add((x, y))
+
 for i in range(len(points)):
     x1, y1 = points[i]
 
     for j in range(i + 1, len(points)):
         x2, y2 = points[j]
 
-        width = abs(x2 - x1) + 1
-        height = abs(y2 - y1) + 1
+        left = min(x1, x2)
+        right = max(x1, x2)
+        top = min(y1, y2)
+        bottom = max(y1, y2)
 
-        area = width * height
+        allowed = red | green
 
-        if area > maxArea:
-            maxArea = area
+        valid = (
+            (left, top) in allowed and
+            (right, top) in allowed and
+            (left, bottom) in allowed and
+            (right, bottom) in allowed
+        )
+
+        if valid:
+            width = right - left + 1
+            height = bottom - top + 1
+            area = width * height
+
+            if area > maxArea:
+                maxArea = area
 
 print("Largest area:", maxArea)
